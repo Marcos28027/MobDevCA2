b3550120b65846c195fd34d8f29642a42be28de1
Marcos28027 <marcos_tyr@hotmail.com>
2023-06-19 14:48:18 +0100

    API connected

---
+++ CA2NewsApp/src/app/getdata.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@capacitor-community/http';
+import { from } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetdataService {
+
+  constructor() { }
+
+   doGet = () => {
+    const options = {
+    //  url: 'https://newsapi.org/v2/everything?q=coding&sortBy=publishedAt&apiKey=20e4357d435b4f1e904a1018ffad5931',
+      url: 'https://newsapi.org/v2/top-headlines?country=ie&apiKey=20e4357d435b4f1e904a1018ffad5931',
+     // headers: { 'X-Fake-Header': 'Max was here' },
+      // params: { size: 'XL' },
+    };
+    return from(Http.get(options))
+  };
+  
+}
