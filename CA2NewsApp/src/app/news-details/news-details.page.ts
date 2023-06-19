121aa4aee8774509f4cd9e0da9d898fe18879c8f
Marcos28027 <marcos_tyr@hotmail.com>
2023-06-19 20:03:19 +0100

    details page with content and link

--- CA2NewsApp/src/app/news-details/news-details.page.ts
+++ CA2NewsApp/src/app/news-details/news-details.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-news-details',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsDetailsPage implements OnInit {
 
-  constructor() { }
+  article: any;
+  constructor(private activatedRoute: ActivatedRoute) {}
+
 
   ngOnInit() {
+    this.article = history.state.article;
   }
 
 }
--- CA2NewsApp/src/app/tab1/tab1.page.ts
+++ CA2NewsApp/src/app/tab1/tab1.page.ts
@@ -1,22 +1,28 @@
 import { Component } from '@angular/core';
 import { GetdataService } from '../getdata.service';
 
+import { NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  
 })
 export class Tab1Page {
-
   data: any;
-  constructor(
-   public getdata : GetdataService
-  ) {}
+  constructor(public getdata: GetdataService, private router: Router) {}
 
-  ngOnInit(){
-this.getdata.doGet().subscribe(res => {
-  this.data = res.data.articles;
-  console.log(this.data);
-})
+  ngOnInit() {
+    this.getdata.doGet().subscribe((res) => {
+      this.data = res.data.articles;
+      console.log(this.data);
+    });
+    
+  }
+  openNewsDetails(article: any) {
+    this.router.navigate(['tabs/tab1/news-details'], { state: { article } });
   }
 }
